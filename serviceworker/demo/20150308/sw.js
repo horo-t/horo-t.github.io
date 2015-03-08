4e78edb823fef90fcebfe7df2ac1e4266631903c
Tsuyoshi Horo <horo@chromium.org>
2015-03-08 11:23:03 +0900

    add mito demo

---
+++ serviceworker/demo/20150308/sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('fetch', function(event) {
+    if (event.request.url.indexOf('test') != -1) {
+      event.respondWith(new Response('Hello world: ' + new Date()));
+    }
+  });
+
