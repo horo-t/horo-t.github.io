24a31a0aa44feab94eee6c1880ec377300ff171e
Tsuyoshi Horo <horo@chromium.org>
2015-04-21 15:48:43 +0900

    add test2

--- serviceworker/demo/tmp/20150421/sw.js
+++ serviceworker/demo/tmp/20150421/sw.js
@@ -9,6 +9,10 @@ self.addEventListener('install', function(event) {
   });
 
 self.addEventListener('fetch', function(e) {
+    if (e.request.url.indexOf('test2.js') != -1) {
+      e.respondWith(new Response('console.log("Hello2!");'));
+      return;
+    }
     e.respondWith( caches.match( e.request ).then(
   		function( response ) {
   			return response || fetch( e.request );
