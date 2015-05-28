2023ab1dbcbe31812326d53a0cc7d85fd4aa088c
Tsuyoshi Horo <horo@chromium.org>
2015-05-28 16:52:18 +0900

    push

--- serviceworker/demo/push/service-worker.js
+++ serviceworker/demo/push/service-worker.js
@@ -1,3 +1,11 @@
+self.addEventListener('activate', function(evt) {
+  if (self.clients && self.clients.claim)
+    self.clients.claim();
+});
+
+if (self.skipWaiting)
+  self.skipWaiting();
+
 self.addEventListener('push', function(event) {
   console.log('Received a push message', event);
 
@@ -14,3 +22,22 @@ self.addEventListener('push', function(event) {
     })
   );
 });
+
+self.addEventListener('notificationclick', function(evt) {
+  evt.notification.close();
+
+  evt.waitUntil(
+    clients.matchAll({ type: 'window' }).then(function(evt) {
+      var p = location.pathname.split('/');
+      p.pop();
+      p = location.protocol + '//' + location.hostname + (location.port ? ':'+location.port : '') + p.join('/') + '/';
+      for(var i = 0 ; i < evt.length ; i++) {
+        var c = evt[i];
+        if(((c.url == p) || (c.url == p + 'index.html')) && ('focus' in c))
+          return c.focus();
+      }
+      if(clients.openWindow)
+        return clients.openWindow('./');
+    })
+  );
+}, false);
