ae0f47ea18267d3cced3a7550cf7ef22be80ba95
Tsuyoshi Horo <horo@chromium.org>
2016-06-01 21:59:27 +0900

    requeststart

--- serviceworker/demo/tmp/20160625/sw.js
+++ serviceworker/demo/tmp/20160625/sw.js
@@ -2,8 +2,8 @@ self.addEventListener('fetch', function(event) {
     if (event.request.url.indexOf('test') != -1) {
       event.respondWith(new Response(
           '<body></body><script>\n' +
-          'document.body.appendChild(document.createTextNode(' +
-          'performance.timing.responseEnd - performance.timing.navigationStart' +
+          'document.body.appendChild(document.createTextNode("requestStart: " + (' +
+          'performance.timing.requestStart - performance.timing.navigationStart)' +
           '))' +
           '</script>\n' ,
           {headers: [['Content-Type', 'Text/Html']]}));
