1e52629314d7d88a8dc993ade0e5851a13860337
Tsuyoshi Horo <horo@chromium.org>
2016-07-15 18:48:03 +0900

    add console.log

--- serviceworker/demo/tmp/20160711/sw.js
+++ serviceworker/demo/tmp/20160711/sw.js
@@ -1,4 +1,5 @@
 self.addEventListener('fetch', function(event) {
+    console.log("fetch event: " + event.request.url);
     if (event.request.url.indexOf('test') != -1) {
       event.respondWith(new Response('Hello world'));
     }
