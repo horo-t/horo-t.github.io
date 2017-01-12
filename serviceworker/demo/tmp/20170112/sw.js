aa20e5a6730e66bce6bdd591be410ab468b14319
Tsuyoshi Horo <horo@chromium.org>
2017-01-12 11:06:42 +0900

    fix demo

--- serviceworker/demo/tmp/20170112/sw.js
+++ serviceworker/demo/tmp/20170112/sw.js
@@ -2,10 +2,10 @@ self.addEventListener('fetch', function(event) {
     if (event.request.url.indexOf('/scope/index.html') != -1) {
       return;
     }
-    var headersText = ""
+    var headersText = "Request headers in SW \n";
     for (var header of event.request.headers) {
       headersText += header[0] + ": " + header[1] + "\n";
     }
-    console.log("Request headers in SW \n" + headersText);
-    event.respondWith(new Response(res));
+    console.log(headersText);
+    event.respondWith(new Response(headersText));
   });
