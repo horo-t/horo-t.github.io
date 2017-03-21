8610a5caf802635ac24097181e6f320c402ca7d6
Tsuyoshi Horo <horo@chromium.org>
2017-03-21 18:45:03 +0900

    20170214 test

--- serviceworker/demo/tmp/20170214/worker.js
+++ serviceworker/demo/tmp/20170214/worker.js
@@ -1,3 +1,15 @@
+// fetch("global fetch");
 self.onmessage = function (msg) {
-  fetch(msg.data).then(res => res.text()).then(text => self.postMessage(text))
+  if (msg.data == 'fetch') {
+    setTimeout(function(){
+        fetch(msg.data)
+          .then(res => res.text())
+          .then(text => self.postMessage(text));
+      }, 100);
+  } else if (msg.data == 'syncXHR') {
+    var request = new XMLHttpRequest();
+    request.open('GET', msg.data, false);  // `false` makes the request synchronous
+    request.send(null);
+    self.postMessage(request.responseText);
+  }
 }
