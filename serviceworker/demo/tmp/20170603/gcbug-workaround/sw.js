8d1f8bd6166070616da8079e90d05ebf279737a3
Tsuyoshi Horo <horo@chromium.org>
2017-06-03 15:31:12 +0900

    add gc bug demo

---
+++ serviceworker/demo/tmp/20170603/gcbug-workaround/sw.js
@@ -0,0 +1,26 @@
+
+self.addEventListener('install', event => {
+  if (!self.registration.navigationPreload) {
+    var err = 'registration.navigationPreload not found';
+    console.error(err);
+    event.waitUntil(Promise.reject(err));
+    return;
+  }
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(registration.navigationPreload.enable());
+});
+
+self.addEventListener('fetch', event => {
+  console.log("fetch event");
+  event.respondWith(event.preloadResponse
+    .then(response => {
+      console.log("preloadResponse resolved");
+      // [HACK] Just to keep .preloadResponse for crbug.com/728013.
+      event.preloadResponse;
+      return response;
+    }));
+  // Need to add --js-flags="--expose-gc" to the flags.
+  setTimeout(_ => {self.gc();}, 0);
+});
---
+++ serviceworker/demo/tmp/20170603/gcbug/sw.js
@@ -0,0 +1,24 @@
+
+self.addEventListener('install', event => {
+  if (!self.registration.navigationPreload) {
+    var err = 'registration.navigationPreload not found';
+    console.error(err);
+    event.waitUntil(Promise.reject(err));
+    return;
+  }
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(registration.navigationPreload.enable());
+});
+
+self.addEventListener('fetch', event => {
+  console.log("fetch event");
+  event.respondWith(event.preloadResponse
+    .then(response => {
+      console.log("preloadResponse resolved");
+      return response;
+    }));
+  // Need to add --js-flags="--expose-gc" to the flags.
+  setTimeout(_ => {self.gc();}, 0);
+});
