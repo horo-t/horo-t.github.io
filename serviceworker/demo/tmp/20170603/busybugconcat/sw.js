0db84a79ab340b26ee037119e79849a78e25a4ab
Tsuyoshi Horo <horo@chromium.org>
2017-06-06 12:22:10 +0900

    busybugconcat

---
+++ serviceworker/demo/tmp/20170603/busybugconcat/sw.js
@@ -0,0 +1,29 @@
+
+self.addEventListener('install', event => {
+  if (!self.registration.navigationPreload) {
+    var err = 'registration.navigationPreload not found';
+    console.error(err);
+    event.waitUntil(Promise.reject(err));
+    return;
+  }
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(registration.navigationPreload.enable());
+});
+
+self.addEventListener('fetch', event => {
+
+  var delayHeaderPromise = new Promise(resolve => {
+    setTimeout(_ => {
+      fetch('./scope/header.html').then(resolve);
+    }, 1000);
+  })
+  event.respondWith(
+    Promise.all([delayHeaderPromise, event.preloadResponse])
+      .then(responses => Promise.all(responses.map(res => res.text())))
+      .then(texts => {
+        return new Response(texts[0] + texts[1]);
+      })
+  );
+});
