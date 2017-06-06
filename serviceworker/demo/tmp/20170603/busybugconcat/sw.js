f8b2af8a6c8ece5cba3fc4c0225db5a1b384f52c
Tsuyoshi Horo <horo@chromium.org>
2017-06-06 12:32:56 +0900

    pendingNavigationRequests

--- serviceworker/demo/tmp/20170603/busybugconcat/sw.js
+++ serviceworker/demo/tmp/20170603/busybugconcat/sw.js
@@ -12,7 +12,14 @@ self.addEventListener('activate', event => {
   event.waitUntil(registration.navigationPreload.enable());
 });
 
+
+self.pendingNavigationRequests = new Set();
+
 self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    self.pendingNavigationRequests.add(event);
+  }
+
   var delayHeaderPromise = new Promise(resolve => {
     setTimeout(_ => {
       fetch('./scope/header.html').then(resolve);
@@ -23,6 +30,9 @@ self.addEventListener('fetch', event => {
     Promise.all([delayHeaderPromise, event.preloadResponse])
       .then(responses => Promise.all(responses.map(res => res.text())))
       .then(texts => {
+        if (event.request.mode === 'navigate') {
+          self.pendingNavigationRequests.delete(event);
+        }
         return new Response(texts[0] + texts[1],
                             {headers: [['content-type', 'text/html']]});
       })
