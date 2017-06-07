242610618225a7de739c99ee23132ed4c324afe3
Tsuyoshi Horo <horo@chromium.org>
2017-06-07 12:31:33 +0900

    install stack demo

---
+++ serviceworker/demo/tmp/20170607/sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('A').then(cache => cache.addAll(['sw.js']))
+    );
+  });
+self.addEventListener('fetch', function(event) {
+    if (event.request.url.indexOf('test') != -1) {
+      event.respondWith(new Response('Hello world'));
+    }
+  });
